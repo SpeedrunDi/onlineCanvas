7f076effe85513c2681e2008f8d178d45f4957d7
SpeedrunDi <amanmaksat0@gmail.com>
2022-10-10 10:31:18 +0600

    made send data and save canvas to serverApi.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,68 @@
-import {useEffect, useRef} from "react";
+import React, {useState, useRef, useEffect} from 'react';
 
 const App = () => {
+  const [state, setState] = useState({
+    mouseDown: false,
+    pixelsArray: []
+  });
+
   const ws = useRef(null);
+  const canvas = useRef(null);
 
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:8000/canvas');
 
 
   }, []);
-  return (
-    <div className="App">
 
+  const canvasMouseMoveHandler = event => {
+    if (state.mouseDown) {
+      const clientX = event.clientX;
+      const clientY = event.clientY;
+      setState(prevState => {
+        return {
+          ...prevState,
+          pixelsArray: [...prevState.pixelsArray, {
+            x: clientX,
+            y: clientY
+          }]
+        };
+      });
+
+      const context = canvas.current.getContext('2d');
+      const imageData = context.createImageData(1, 1);
+      const d = imageData.data;
+
+      d[0] = 0;
+      d[1] = 0;
+      d[2] = 0;
+      d[3] = 255;
+
+      context.putImageData(imageData, event.clientX, event.clientY);
+    }
+  };
+
+  const mouseDownHandler = () => {
+    setState({...state, mouseDown: true});
+  };
+
+  const mouseUpHandler = () => {
+    ws.current.send(JSON.stringify(state.pixelsArray));
+    // Где-то здесь отправлять массив пикселей на сервер
+    setState({...state, mouseDown: false, pixelsArray: []});
+  };
+
+  return (
+    <div>
+      <canvas
+        ref={canvas}
+        style={{border: '1px solid black'}}
+        width={800}
+        height={600}
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
+        onMouseMove={canvasMouseMoveHandler}
+      />
     </div>
   );
 };
--- serverApi/index.js
+++ serverApi/index.js
@@ -10,11 +10,23 @@ const port = 8000;
 app.use(cors());
 
 const activeConnections = {};
+const arrayCanvas = [];
 
-app.ws('/canvas', (ws, req) => {
+app.ws('/canvas', (ws) => {
   const id = nanoid();
 
   console.log('Client connected id=', id);
+  activeConnections[id] = ws;
+
+  ws.on('close', () => {
+    console.log('Client disconnected! id=', id);
+    delete activeConnections[id];
+  });
+
+  ws.on('message', array => {
+    arrayCanvas.push(...JSON.parse(array));
+    console.log(arrayCanvas);
+  });
 });
 
 app.listen(port, () => {
