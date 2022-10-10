e44a21583cb9432da5549de2fc8276ce175f6721
SpeedrunDi <amanmaksat0@gmail.com>
2022-10-10 12:13:38 +0600

    added color selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
     mouseDown: false,
     pixelsArray: []
   });
+  const [color, setColor] = useState([0, 0, 0, 255]);
 
   const ws = useRef(null);
   const canvas = useRef(null);
@@ -40,15 +41,15 @@ const App = () => {
     const imageData = context.createImageData(1, 1);
     const d = imageData.data;
 
-    d[0] = 0;
-    d[1] = 0;
-    d[2] = 0;
-    d[3] = 255;
-
     state.pixelsArray.forEach(data => {
+      d[0] = data.color[0];
+      d[1] = data.color[1];
+      d[2] = data.color[2];
+      d[3] = data.color[3];
+
       context.putImageData(imageData, data.x, data.y)
     });
-  }, [state.pixelsArray]);
+  }, [state.pixelsArray, color]);
 
   const canvasMouseMoveHandler = event => {
     if (state.mouseDown) {
@@ -59,7 +60,8 @@ const App = () => {
           ...prevState,
           pixelsArray: [...prevState.pixelsArray, {
             x: clientX,
-            y: clientY
+            y: clientY,
+            color: color
           }]
         };
       });
@@ -68,10 +70,10 @@ const App = () => {
       const imageData = context.createImageData(1, 1);
       const d = imageData.data;
 
-      d[0] = 0;
-      d[1] = 0;
-      d[2] = 0;
-      d[3] = 255;
+      d[0] = color[0];
+      d[1] = color[1];
+      d[2] = color[2];
+      d[3] = color[3];
 
       context.putImageData(imageData, event.clientX, event.clientY);
     }
@@ -90,8 +92,21 @@ const App = () => {
     setState({...state, mouseDown: false, pixelsArray: []});
   };
 
+  const changeColor = (e) => {
+    const currentColor = e.target.name
+    if (currentColor === 'red') {
+      setColor([255, 0, 0, 255]);
+    } else if (currentColor === 'green') {
+      setColor([0, 255, 0, 255]);
+    } else if (currentColor === 'black') {
+      setColor([0, 0, 0, 255]);
+    } else if (currentColor === 'blue') {
+      setColor([0, 0, 255, 255]);
+    }
+  };
+
   return (
-    <div>
+    <div style={{display: "flex"}}>
       <canvas
         ref={canvas}
         style={{border: '1px solid black'}}
@@ -101,6 +116,41 @@ const App = () => {
         onMouseUp={mouseUpHandler}
         onMouseMove={canvasMouseMoveHandler}
       />
+      <div style={{position: "relative", padding: "30px"}}>
+        <p style={{position: "relative", marginTop: "0"}}>current color:
+          <span
+            style={{
+              position: "absolute",
+              width: "20px",
+              height: "20px",
+              backgroundColor: `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`,
+              border: "0",
+              top: "0",
+              marginLeft: "10px"
+          }}
+          />
+        </p>
+        <button
+          style={{position: "absolute", width: "20px", height: "20px", background: "black", border: "0", top: "60px"}}
+          name="black"
+          onClick={changeColor}
+        />
+        <button
+          style={{position: "absolute", width: "20px", height: "20px", background: "green", border: "0", top: "90px"}}
+          name="green"
+          onClick={changeColor}
+        />
+        <button
+          style={{position: "absolute", width: "20px", height: "20px", background: "blue", border: "0", top: "120px"}}
+          name="blue"
+          onClick={changeColor}
+        />
+        <button
+          style={{position: "absolute", width: "20px", height: "20px", background: "red", border: "0", top: "150px"}}
+          name="red"
+          onClick={changeColor}
+        />
+      </div>
     </div>
   );
 };
